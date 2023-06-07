7d713df43d9de492b1ba93a99fc4cbbef42b92ee
yasmeenhalawani <129608923+yasmeenhalawani@users.noreply.github.com>
2023-06-07 13:55:20 -0500

    Update settings.py

--- Blog-Project-main/src/cblog/settings.py
+++ Blog-Project-main/src/cblog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import Config
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = Config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
