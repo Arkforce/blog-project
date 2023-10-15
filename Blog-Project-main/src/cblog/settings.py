8b516b01b36eb2f1f58561fd31333b7f59dd47e6
Arkforce <145881267+Arkforce@users.noreply.github.com>
2023-10-14 23:45:06 -0500

    Update settings.py

--- Blog-Project-main/src/cblog/settings.py
+++ Blog-Project-main/src/cblog/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'database1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': 'Tech1234',
-        'HOST': 'database1.caumocpqz6im.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'HOST': 'database1.ce3l9gxhoovh.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
